2b2858c07635760747d64b81a2a822680f2f6981
Malcolm Garner <malcolm@Malcolms-MacBook-Pro.local>
2022-03-01 22:51:05 +0000

    gatsby looking around

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import Layout from "../components/Layout"
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>Design</h2>
+          <h3>Follow on tut</h3>
+          <p>
+            Take it to 22 vids!
+          </p>
+        </div>
+      </section>
+    </Layout>
+  )
 }
